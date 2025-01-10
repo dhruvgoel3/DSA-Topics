682ae03faf92f719d8441eca97256e5d63704af4
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-01-10 16:42:27 +0530

    changes

---
+++ Arrays/array_2D.java
@@ -0,0 +1,22 @@
+// @ dimenssiinal array is like a table with rows and columns
+// @ 2D array is an array of arrays
+// @ 2D array is a matrix
+// @ 2D array is a list of lists
+// @ 2D array is a collection of arrays
+// @ 2D array is a collection of collections
+// @ 2D array is a collection of rows and columns
+// @ 2D array is a collection of elements
+// @ 2D array is a collection of cells
+// @ 2D array is a collection of values
+// @ 2D array is a collection of items
+// @ 2D array is a collection of objects
+
+// This is waht we can give as a definition of 2D array
+
+public class array_2D {
+    public static void main(String[] args) {
+        int A[][] = new int[3][3];
+
+    }
+
+}
