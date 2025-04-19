31a8d319a7188ccbd5d1a3c61c90b1bf42469f9c
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-04-19 10:48:45 +0530

    dsa

---
+++ Arrays/two_dims.java
@@ -0,0 +1,16 @@
+//  Ways of initializing a 2D array in Java
+public class two_dims {
+    public static void main(String[] args) {
+        int[][] Array = new int[3][3];
+        int[][] MyArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        System.out.println(MyArray[0][0]); // 1
+        System.out.println(MyArray[0][1]); // 1
+        System.out.println(MyArray[0][2]);
+
+        System.out.println(MyArray[1][0]);// 1
+        System.out.println(MyArray[1][1]);// 1
+        System.out.println(MyArray[1][2]);// 1
+    }
+
+}
